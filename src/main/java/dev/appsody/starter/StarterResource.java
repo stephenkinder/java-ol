7e203d753fe4fb31bcf023a93abeeb9b5bc45ffc
Stephen Kinder <aasjkaa@gmail.com>
2020-03-28 10:31:02 -0400

    appsody init

---
+++ src/main/java/dev/appsody/starter/StarterResource.java
@@ -0,0 +1,37 @@
+package dev.appsody.starter;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.lang.System;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Path("/")
+public class StarterResource {
+
+    @Path("version")
+    @GET
+    public Response getVersion() {
+         
+    String libertyVersion="";
+    try (InputStream input = new FileInputStream(System.getProperty("wlp.install.dir") + "/lib/versions/openliberty.properties")) {
+
+        Properties prop = new Properties();
+
+        // load a properties file
+        prop.load(input);
+
+        // get the property value and print it out
+        libertyVersion=prop.getProperty("com.ibm.websphere.productVersion");
+
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+
+        return Response.ok("Hello, World! Hosted by Open Liberty v("+libertyVersion+")")
+                .build();
+    }
+}
